bd11b5c4dd5a7a4c91d41dcb3880b9d04af76885
Rogerio Melo <rogerio@inovebr.com>
2021-06-24 11:10:23 -0300

    Add project files.

---
+++ ConsoleApp2/Mensagem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Mensagem
+    {
+        //    private string TextoMensagem;
+
+        //    public void ExibirMensagem()
+        //    {
+        //        Console.WriteLine(this.TextoMensagem);
+        //    }
+        //    public string getTextoMensagem()
+        //    {
+        //        return this.TextoMensagem;
+        //    }   
+        //            public void setTextoMensagem(string valor)
+        //    {
+        //this.TextoMensagem = valor.ToUpper();
+
+        private string textoMensagem;
+        public string TextoMensagem
+        {
+            get
+            {
+                return this.textoMensagem;
+            }
+            set
+            {
+                this.textoMensagem = value.ToUpper();
+            }
+        }
+        public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem);
+        }
+    }
+}
+    
+
+    
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem msg1, msg2;
+
+            msg1 = new Mensagem();
+
+            msg1.TextoMensagem = "acorda vagaba";
+            msg1.ExibirMensagem();
+            
+
+            Console.ReadKey();
+        }
+    }
+}
